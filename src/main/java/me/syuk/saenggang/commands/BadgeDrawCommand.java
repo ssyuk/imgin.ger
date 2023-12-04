7696fd8d564559e847989207a442c47f8a8bbeb8
ssyuk <sihu@syuk.me>
2023-12-04 22:00:57 +0900

    뱃지 코인 변경

--- src/main/java/me/syuk/saenggang/commands/BadgeDrawCommand.java
+++ src/main/java/me/syuk/saenggang/commands/BadgeDrawCommand.java
@@ -17,14 +17,14 @@ public class BadgeDrawCommand implements Command {
 
     @Override
     public void execute(DBManager.Account account, String[] args, Message message) {
-        if (account.coin() < 300) {
+        if (account.coin() < 100) {
             message.reply("코인이 부족합니다.");
             return;
         }
 
         int badgeId = DBManager.drawBadge(account);
         message.reply("뽑은 뱃지: " + Utils.getBadge(badgeId));
-        account.giveCoin(message.getChannel(), -300, "뱃지뽑기에");
+        account.giveCoin(message.getChannel(), -100, "뱃지뽑기에");
         DBManager.addBadge(account, badgeId);
     }
 }
