cd3c8b4765530bff54b6252827238277850faf72
ssyuk <sihu@syuk.me>
2023-12-04 22:34:16 +0900

    확률조정

--- src/main/java/me/syuk/saenggang/commands/GamblingCommand.java
+++ src/main/java/me/syuk/saenggang/commands/GamblingCommand.java
@@ -71,18 +71,18 @@ public class GamblingCommand implements Command {
                     reply.append("\uD83C\uDF89 축하해요! ").append(Utils.displayCoin((int) (coins * 0.5))).append("을 얻었어요! (1.5배)");
                     DBManager.giveCoin(account, (int) (coins * .5));
                 }
-                case 2, 3, 4, 5 -> {
+                case 2, 3, 4 -> {
                     reply.append("\uD83C\uDF89 축하해요! ").append(Utils.displayCoin((int) (coins * 0.3))).append("을 얻었어요! (1.3배)");
                     DBManager.giveCoin(account, (int) (coins * .3));
                 }
-                case 6 -> reply.append("코인을 그대로 돌려받았어요! (1배)");
-                case 7, 8 -> {
+                case 5 -> reply.append("코인을 그대로 돌려받았어요! (1배)");
+                case 6, 7, 8 -> {
                     reply.append("\uD83D\uDC94 ").append(Utils.displayCoin((int) (coins * .3))).append("을 잃었어요ㅠ (0.7배)");
                     DBManager.giveCoin(account, (int) -(coins * .3));
                 }
                 case 9 -> {
-                    reply.append("\uD83C\uDF29 ").append(Utils.displayCoin((int) (coins * .8))).append("을 잃었어요ㅠ (0.2배)");
-                    DBManager.giveCoin(account, (int) -(coins * .8));
+                    reply.append("\uD83C\uDF29 ").append(Utils.displayCoin((int) (coins * .8))).append("을 잃었어요ㅠ (0.5배)");
+                    DBManager.giveCoin(account, (int) -(coins * .5));
                 }
             }
             reply.append("\n").append("현재 코인: ").append(Utils.displayCoin(account.coin()));
