abac4f5e0144e3fb258bc75d145020ad5fa8e7c2
ssyuk <sihu@syuk.me>
2023-12-23 20:40:56 +0900

    빡세게 공부해서 중간고사 만점

--- src/main/java/me/syuk/saenggang/MessageCreated.java
+++ src/main/java/me/syuk/saenggang/MessageCreated.java
@@ -16,6 +16,7 @@ import org.javacord.api.listener.message.MessageCreateListener;
 import org.javacord.api.util.NonThrowingAutoCloseable;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,97 +43,99 @@ public class MessageCreated implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        Optional<User> oUser = event.getMessageAuthor().asUser();
-        if (oUser.isEmpty()) return;
+        CompletableFuture.runAsync(() -> {
+            Optional<User> oUser = event.getMessageAuthor().asUser();
+            if (oUser.isEmpty()) return;
 
-        User user = oUser.get();
-        if (Utils.isBot(user)) return;
+            User user = oUser.get();
+            if (Utils.isBot(user)) return;
 
-        DBManager.Account account = DBManager.getAccount(user);
+            DBManager.Account account = DBManager.getAccount(user);
 
-        String content = event.getMessageContent();
-        Message message = event.getMessage();
+            String content = event.getMessageContent();
+            Message message = event.getMessage();
 
-        if (replyCallbackMap.containsKey(account)) {
-            ReplyCallback callback = replyCallbackMap.get(account);
-            if (callback.onReply(message)) return;
-        }
-
-        if (content.equals("생강아")) {
-            message.reply("안녕하세요! 생강이에요.\n" +
-                    "`생강아 [할말]`로 저에게 말을 걸 수 있어요.\n" +
-                    "`생강아 배워 [명령어] [메시지]`로 저에게 말을 가르칠 수 있어요!"
-                    + (account.coin() == 0 ? "\n앞으로 저와 재미있게 놀아봐요!" : ""));
-            if (account.coin() == 0) account.giveCoin(message.getChannel(), 5, "첫 사용자 보상으로");
-            return;
-        }
-        if (!content.startsWith("생강아 ")) return;
-        content = content.substring(4);
-
-        List<String> args = new ArrayList<>();
-        Pattern regex = Pattern.compile("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'");
-        Matcher regexMatcher = regex.matcher(content);
-        while (regexMatcher.find()) {
-            if (regexMatcher.group(1) != null) {
-                args.add(regexMatcher.group(1));
-            } else if (regexMatcher.group(2) != null) {
-                args.add(regexMatcher.group(2));
-            } else {
-                args.add(regexMatcher.group());
-            }
-        }
-        Command command = Command.findCommand(args.get(0));
-        if (command != null) {
-            command.execute(account, args.toArray(String[]::new), message);
-            return;
-        }
-
-        List<DBManager.SaenggangKnowledge> knowledge = DBManager.getKnowledge(content);
-
-        if (knowledge.isEmpty()) {
-            NonThrowingAutoCloseable typing = message.getChannel().typeContinuously();
-
-            JsonArray contents = new JsonArray();
-
-            try {
-                Optional<MessageReference> oReferenced = message.getMessageReference();
-                if (oReferenced.isPresent()) {
-                    Message referenced = oReferenced.get().requestMessage().orElseThrow().get();
-                    if (referenced.getAuthor().isYourself()) {
-                        Optional<MessageReference> oReferencedReferenced = referenced.getMessageReference();
-                        if (oReferencedReferenced.isPresent()) {
-                            Message referencedReferenced = oReferencedReferenced.get().requestMessage().orElseThrow().get();
-                            contents.add(AI.generateContent("user", referencedReferenced.getContent()));
-                            contents.add(AI.generateContent("model", referenced.getContent()));
-                        }
-                    }
-                }
-            } catch (Exception ignored) {
+            if (replyCallbackMap.containsKey(account)) {
+                ReplyCallback callback = replyCallbackMap.get(account);
+                if (callback.onReply(message)) return;
             }
 
-
-            String answer = AI.generateResponse(content, contents);
-            typing.close();
-            if (answer == null) {
-                message.reply("ㄴ네..? 뭐라구요?\n" +
-                        "`생강아 배워 \"[명령어]\" \"[메시지]\"`로 알려주세요!");
+            if (content.equals("생강아")) {
+                message.reply("안녕하세요! 생강이에요.\n" +
+                        "`생강아 [할말]`로 저에게 말을 걸 수 있어요.\n" +
+                        "`생강아 배워 [명령어] [메시지]`로 저에게 말을 가르칠 수 있어요!"
+                        + (account.coin() == 0 ? "\n앞으로 저와 재미있게 놀아봐요!" : ""));
+                if (account.coin() == 0) account.giveCoin(message.getChannel(), 5, "첫 사용자 보상으로");
                 return;
             }
-            UUID aiReplyId = UUID.randomUUID();
-            new MessageBuilder().setContent(fixAnswer(answer, account) + "\n" +
-                    "`* AI가 생성한 메시지에요. 올바르지 않은 정보가 담겨있을 수 있어요.`\n" +
-                    "`생강아 배워 \"[명령어]\" \"[메시지]\"`로 새로운 지식을 가르쳐주세요!")
-                    .addComponents(ActionRow.of(
-                            Button.create("ai-regenerate-" + aiReplyId, ButtonStyle.PRIMARY, "다시 생성하기", "🔄")
-                    ))
-                    .replyTo(message).send(message.getChannel()).whenComplete((message1, throwable) -> AI.aiReplyMap.put(aiReplyId, message1));
-            return;
-        }
+            if (!content.startsWith("생강아 ")) return;
+            content = content.substring(4);
 
-        DBManager.SaenggangKnowledge selectedKnowledge = knowledge.get((int) (Math.random() * knowledge.size()));
-        String answer = fixAnswer(selectedKnowledge.answer(), account);
-        message.reply(answer + "\n" +
-                "`" + selectedKnowledge.authorName() + "님이 알려주셨어요.`");
+            List<String> args = new ArrayList<>();
+            Pattern regex = Pattern.compile("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'");
+            Matcher regexMatcher = regex.matcher(content);
+            while (regexMatcher.find()) {
+                if (regexMatcher.group(1) != null) {
+                    args.add(regexMatcher.group(1));
+                } else if (regexMatcher.group(2) != null) {
+                    args.add(regexMatcher.group(2));
+                } else {
+                    args.add(regexMatcher.group());
+                }
+            }
+            Command command = Command.findCommand(args.get(0));
+            if (command != null) {
+                command.execute(account, args.toArray(String[]::new), message);
+                return;
+            }
+
+            List<DBManager.SaenggangKnowledge> knowledge = DBManager.getKnowledge(content);
+
+            if (knowledge.isEmpty()) {
+                NonThrowingAutoCloseable typing = message.getChannel().typeContinuously();
+
+                JsonArray contents = new JsonArray();
+
+                try {
+                    Optional<MessageReference> oReferenced = message.getMessageReference();
+                    if (oReferenced.isPresent()) {
+                        Message referenced = oReferenced.get().requestMessage().orElseThrow().get();
+                        if (referenced.getAuthor().isYourself()) {
+                            Optional<MessageReference> oReferencedReferenced = referenced.getMessageReference();
+                            if (oReferencedReferenced.isPresent()) {
+                                Message referencedReferenced = oReferencedReferenced.get().requestMessage().orElseThrow().get();
+                                contents.add(AI.generateContent("user", referencedReferenced.getContent()));
+                                contents.add(AI.generateContent("model", referenced.getContent()));
+                            }
+                        }
+                    }
+                } catch (Exception ignored) {
+                }
+
+
+                String answer = AI.generateResponse(content, contents);
+                typing.close();
+                if (answer == null) {
+                    message.reply("ㄴ네..? 뭐라구요?\n" +
+                            "`생강아 배워 \"[명령어]\" \"[메시지]\"`로 알려주세요!");
+                    return;
+                }
+                UUID aiReplyId = UUID.randomUUID();
+                new MessageBuilder().setContent(fixAnswer(answer, account) + "\n" +
+                                "`* AI가 생성한 메시지에요. 올바르지 않은 정보가 담겨있을 수 있어요.`\n" +
+                                "`생강아 배워 \"[명령어]\" \"[메시지]\"`로 새로운 지식을 가르쳐주세요!")
+                        .addComponents(ActionRow.of(
+                                Button.create("ai-regenerate-" + aiReplyId, ButtonStyle.PRIMARY, "", "🔄")
+                        ))
+                        .replyTo(message).send(message.getChannel()).whenComplete((message1, throwable) -> AI.aiReplyMap.put(aiReplyId, message1));
+                return;
+            }
+
+            DBManager.SaenggangKnowledge selectedKnowledge = knowledge.get((int) (Math.random() * knowledge.size()));
+            String answer = fixAnswer(selectedKnowledge.answer(), account);
+            message.reply(answer + "\n" +
+                    "`" + selectedKnowledge.authorName() + "님이 알려주셨어요.`");
+        });
     }
 
     public interface ReplyCallback {
